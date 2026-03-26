8f78463235542c12e6596bf26a8c51a72612cf62
Mailson <mailsonestudo48@gmail.com>
2026-03-25 21:51:26 -0300

    primeiro app

--- app/modal.tsx
+++ app/modal.tsx
@@ -1,29 +1,82 @@
-import { Link } from 'expo-router';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, Alert } from "react-native";
 
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
+export default function App() {
 
-export default function ModalScreen() {
+  const mostrarAlerta = () => {
+    Alert.alert("Mensagem", "Bem-vindo ao meu aplicativo!");
+  };
+
   return (
-    <ThemedView style={styles.container}>
-      <ThemedText type="title">This is a modal</ThemedText>
-      <Link href="/" dismissTo style={styles.link}>
-        <ThemedText type="link">Go to home screen</ThemedText>
-      </Link>
-    </ThemedView>
+    <View style={styles.container}>
+
+      <Image
+        source={require("../assets/images/foto.jpg")}
+        style={styles.imagem}
+      />
+
+      <Text style={styles.nome}>
+        Mailson Marques
+      </Text>
+
+      <Text style={styles.curso}>
+        Sistemas para Internet
+      </Text>
+
+      <Text style={styles.frase}>
+        "Persista e insista, o resultado virá!"
+      </Text>
+
+      <View style={styles.botao}>
+        <Button
+          title="Clique aqui"
+          onPress={mostrarAlerta}
+        />
+      </View>
+
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#f2f2f2",
+    alignItems: "center",
+    justifyContent: "center",
     padding: 20,
   },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
+
+  imagem: {
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+    marginBottom: 20,
   },
+
+  nome: {
+    fontSize: 26,
+    fontWeight: "bold",
+    color: "#333",
+  },
+
+  curso: {
+    fontSize: 18,
+    color: "#555",
+    marginTop: 5,
+  },
+
+  frase: {
+    fontSize: 16,
+    fontStyle: "italic",
+    textAlign: "center",
+    marginVertical: 20,
+    color: "#666",
+  },
+
+  botao: {
+    marginTop: 10,
+    width: "60%",
+  }
+
 });
